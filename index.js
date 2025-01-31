678a1709a3687894f5951192e697a07ce60bcba5
THINK BOOK <dhairya.jangir.s73@kalvium.community>
2025-01-31 13:08:10 +0530

    assignment done

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
 const express = require('express');
 const { resolve } = require('path');
+const mongoose = require('mongoose')
+require('dotenv').config();
+
 
 const app = express();
-const port = 3010;
+const PORT =  3010;
 
-app.use(express.static('static'));
+const URL = process.env.MONGO_URI || "mongodb+srv://dhairyajangirs73:dhairya@cluster0.y4vnu.mongodb.net/";
+
+
+app.use(express.static(resolve(__dirname, 'static')));
+mongoose.connect(URL, { useNewUrlParser: true, useUnifiedTopology: true }).then(()=>console.log(`Database connected`)).catch((err)=>console.log(`Error connecting DB:${err}`))
 
 app.get('/', (req, res) => {
-  res.sendFile(resolve(__dirname, 'pages/index.html'));
+  res.sendFile(resolve(__dirname, 'pages', 'index.html')); 
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
 });
